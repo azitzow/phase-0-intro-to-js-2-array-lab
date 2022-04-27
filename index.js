26b950077fe63fbb65963a4853fd55273c39c989
Ana Zitzow <anasempai89@gmail.com>
2022-04-27 14:40:09 -0700

    commit tree 5cbf871ecb2dc3f39fedc1cc7ec60b342b0aa541

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+const destructivelyAppendCat = (name) => cats.push(name);
+
+const destructivelyPrependCat = (name) => cats.unshift(name);
+
+const destructivelyRemoveLastCat = () => cats.pop();
+
+const destructivelyRemoveFirstCat = () => cats.shift();
+
+const appendCat = (name) => {
+  const addToEnd = [...cats, 'Broom'];
+  return addToEnd;
+}
+
+const prependCat = (name) => {
+  const addToBeginning = ['Arnold', ...cats];
+  return addToBeginning;
+}
+
+const removeLastCat = () => cats.slice(0, cats.length - 1);
+
+const removeFirstCat = () => cats.slice(1);
+
